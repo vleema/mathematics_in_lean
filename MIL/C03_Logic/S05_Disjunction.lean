57bf87ba1aa95f79b86b32b518a9a7ac13a790cc
Vinicius de Lima <vldsampaio@pm.me>
2025-05-01 16:36:26 -0300

    C03.S05

--- MIL/C03_Logic/S05_Disjunction.lean
+++ MIL/C03_Logic/S05_Disjunction.lean
@@ -46,7 +46,7 @@ example : x < |y| → x < y ∨ x < -y := by
     rw [abs_of_neg h]
     intro h; right; exact h
 
-example : x < |y| → x < y ∨ x < -y := by
+lemma lemma1 : x < |y| → x < y ∨ x < -y := by
   match le_or_gt 0 y with
     | Or.inl h =>
       rw [abs_of_nonneg h]
@@ -58,19 +58,45 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+  match le_or_gt 0 x with
+    | Or.inl h =>
+      rw [abs_of_nonneg h]
+    | Or.inr h =>
+      rw [abs_of_neg h]
+      exact le_of_lt_or_eq $ Or.inl $ lt_trans h $ neg_pos_of_neg h
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+  have h := (le_abs_self (-x))
+  rw [abs_neg] at h
+  exact h
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rcases le_or_gt 0 (x + y) with h | h
+  · rw [abs_of_nonneg h]
+    exact add_le_add (le_abs_self x) (le_abs_self y)
+  · rw [abs_of_neg h]; ring_nf
+    exact add_le_add (neg_le_abs_self x) (neg_le_abs_self y)
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+  constructor
+  · intro h
+    exact lemma1 h
+  · intro h
+    rcases h with hp | hn
+    · exact lt_of_lt_of_le hp (le_abs_self y)
+    · exact lt_of_lt_of_le hn (neg_le_abs_self y)
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  constructor
+  · intro h
+    rw [neg_lt]
+    exact ⟨lt_of_le_of_lt (neg_le_abs_self x) h, lt_of_le_of_lt (le_abs_self x) h⟩
+  · intro h
+    rcases le_or_gt 0 x with h' | h'
+    · rw [abs_of_nonneg h']
+      exact h.2
+    · rw [abs_of_neg h', neg_lt]
+      exact h.1
 
 end MyAbs
 
@@ -91,23 +117,55 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨x, y, h' | h'⟩ <;> linarith [pow_two_nonneg x, pow_two_nonneg y]
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : x ^ 2 - 1 = 0 := by rw [h, sub_self]
+  have h'' : (x + 1) * (x - 1) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h''' | h'''
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h'''
+  · left
+    exact eq_of_sub_eq_zero h'''
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h' : x ^ 2 - y ^ 2 = 0 := by rw [h, sub_self]
+  have h'' : (x + y) * (x - y) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h''' | h'''
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h'''
+  · left
+    exact eq_of_sub_eq_zero h'''
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : x ^ 2 - 1 = 0 := by rw [h, sub_self]
+  have h'' : (x + 1) * (x - 1) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h''' | h'''
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h'''
+  · left
+    exact eq_of_sub_eq_zero h'''
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h' : x ^ 2 - y ^ 2 = 0 := by rw [h, sub_self]
+  have h'' : (x + y) * (x - y) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h''' | h'''
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h'''
+  · left
+    exact eq_of_sub_eq_zero h'''
 
 end
 
@@ -124,5 +182,13 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
-
+  constructor
+  · intro h
+    by_cases h' : P
+    · exact Or.inr (h h')
+    · exact Or.inl h'
+  · intro h
+    rcases h with h' | h'
+    · intro h''
+      contradiction
+    · intro _; exact h'
